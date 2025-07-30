c523219883aa046c12136af86bb153647974c9e2
thohui <contact@thohui.com>
2025-07-30 23:47:18 +0200

    fix images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+	image: {
+		service: passthroughImageService()
+	},
 
   integrations: [react()]
 });
