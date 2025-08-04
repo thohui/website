7a6145284dce1a9da5dfa23a720e36eb36c1e01b
thohui <contact@thohui.com>
2025-08-04 18:17:55 +0200

    newwwww design :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
 	image: {
 		service: passthroughImageService()
 	},
-
-  integrations: [react()]
 });
