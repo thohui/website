1ff5518e3a649b12b483cf0a50e187075429fcd4
thohui <contact@thohui.com>
2025-08-28 17:11:10 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'never',
+  site: 'https://thohui.com',
+  integrations: [sitemap()],
   image: {
     service: passthroughImageService()
   },
