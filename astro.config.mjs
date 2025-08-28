6be380cc57879e0438d808dab7dfdea67fd008a4
thohui <contact@thohui.com>
2025-08-28 17:07:07 +0200

    remove ugly trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'never',
   image: {
-      service: passthroughImageService()
-	},
-
+    service: passthroughImageService()
+  },
 });
