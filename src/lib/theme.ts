10fea61e5c00422778d8d4c295c3c99f8d1a1dd8
thohui <contact@thohui.com>
2025-09-09 18:23:41 +0200

    add theme switcher

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,23 +1,16 @@
-const THEME_KEY = "theme";
+export const THEME_KEY = "theme";
 
-type Theme = "dark" | "light";
+export type Theme = "dark" | "light";
 
 const root = document.documentElement;
 
 export function getTheme() {
 	const value = localStorage.getItem(THEME_KEY) === "dark" ? "dark" : "light";
 	if (!value) {
-		setTheme("light");
 		return "light";
 	}
 	return value;
 };
-
-
-export function initTheme() {
-	const theme = getTheme();
-	root.setAttribute("data-theme", theme);
-}
 
 export function setTheme(theme: Theme) {
 	localStorage.setItem(THEME_KEY, theme);
